11ee8e475696480c770e725f39ae93943b2df80e
Klashe <officialklashed@gmail.com>
2019-04-18 16:59:44 +0200

    Edited private conversations_controller

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -2,22 +2,8 @@ class Private::ConversationsController < ApplicationController
   
   def create
     recipient_id = Book.find(params[:book_id]).user.id
-    if params[:recipient_id]
-      @recipient = User.find_by(id: params[:recipient_id])
-      redirect_to(root_path) and return unless @recipient
-      @conversation = Private::Conversation.between(current_user.id, @recipient.id)[0]
-    end
     @conversation = Private::Conversation.new(sender_id: current_user.id, 
                                              recipient_id: recipient_id)
-
-    if params[:recipient_id]
-      @recipient = User.find_by(id: params[:recipient_id])
-      redirect_to(root_path) and return unless @recipient
-      @conversation = Private::Conversation.between(current_user.id, @recipient.id)[0]
-    else
-      @conversation = Private::Conversation.find_by(id: params[:conversation_id])
-      redirect_to(root_path) and return unless @conversation && @conversation.participates?(current_user)
-    end
     if @conversation.save
       Private::Message.create(user_id: current_user.id, 
                               conversation_id: @conversation.id, 
@@ -45,7 +31,6 @@ class Private::ConversationsController < ApplicationController
   end
 
   def open
-    if params[:recipient_id]
     @conversation = Private::Conversation.find(params[:id])
     add_to_conversations unless already_added?
     respond_to do |format|
